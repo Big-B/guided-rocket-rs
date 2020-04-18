66c9b04de7cb29a68bf2645d6540084f14b6b1b3
Brandon Mittman <BrandonMittman@gmail.com>
2020-04-17 22:24:33 -0500

    Update to newer libraries

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 #![no_std]
 #![no_main]
 #![feature(never_type)]
+extern crate arduino_mkrzero as hal;
 
-use arduino_mkrzero::clock::GenericClockController;
-use arduino_mkrzero::delay::Delay;
-use arduino_mkrzero::prelude::*;
-use arduino_mkrzero::sercom::*;
-use arduino_mkrzero::{entry, CorePeripherals, Peripherals};
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::prelude::*;
+use hal::sercom::*;
+use hal::entry;
+use hal::pac::{CorePeripherals, Peripherals};
 use core::fmt::Write;
-use mpu6050::{Addr, AfSel, FsSel, Mpu6050, Mpu6050Error};
+use mpu6050::{Mpu6050, Steps};
 
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-    let mut core = CorePeripherals::take().unwrap();
+    let core = CorePeripherals::take().unwrap();
     let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.GCLK,
         &mut peripherals.PM,
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
-    let mut pins = arduino_mkrzero::Pins::new(peripherals.PORT);
+    let mut pins = hal::Pins::new(peripherals.PORT);
     let mut led = pins.led_builtin.into_open_drain_output(&mut pins.port);
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
@@ -39,12 +41,8 @@ fn main() -> ! {
         &clocks.sercom5_core(&gclk0).unwrap(),
         115_200.hz(),
         peripherals.SERCOM5,
-        &mut core.NVIC,
         &mut peripherals.PM,
-        UART5Pinout::Rx3Tx2 {
-            rx: rx_pin,
-            tx: tx_pin,
-        },
+        (rx_pin, tx_pin)
     );
 
     // This delay seems to be necessary to ensure the next print statement
@@ -53,7 +51,10 @@ fn main() -> ! {
     writeln!(serial, "=======ENTER=======").unwrap();
 
     // Setup I2C port
-    let i2c = I2CMaster0::new(
+    let i2c: I2CMaster0<
+        hal::sercom::Sercom0Pad0<hal::gpio::Pa8<hal::gpio::PfC>>,
+        hal::sercom::Sercom0Pad1<hal::gpio::Pa9<hal::gpio::PfC>>,
+    > = I2CMaster0::new(
         &clocks.sercom0_core(&gclk0).unwrap(),
         400.khz(),
         peripherals.SERCOM0,
@@ -63,25 +64,14 @@ fn main() -> ! {
     );
 
     // Instantiate imu instance
-    let imu = Mpu6050::new(i2c, Addr::LOW, AfSel::PlusMinus2G, FsSel::PlusMinus250DPS);
-    match imu {
-        Ok(mut imu) => loop {
-            let gyro = imu.gyro_in_dps().unwrap();
-            let accl = imu.accel_in_g().unwrap();
-            let temp = imu.temp_in_deg_c().unwrap();
+    let mut imu = Mpu6050::new(i2c);
+    imu.init().unwrap();
+    imu.soft_calib(Steps(100)).unwrap();
 
-            writeln!(serial, "gyro - x: {}, y: {}, z: {}", gyro.x, gyro.y, gyro.z).unwrap();
-            writeln!(serial, "accl - x: {}, y: {}, z: {}", accl.x, accl.y, accl.z).unwrap();
-            writeln!(serial, "temp: {}", temp).unwrap();
-
-            led.set_low();
-            delay.delay_ms(250u32);
-            led.set_high();
-            delay.delay_ms(250u32);
-        },
-        Err(Mpu6050Error::I2c(_)) => writeln!(serial, "I2C Error").unwrap(),
-        Err(Mpu6050Error::BadIdentifier) => writeln!(serial, "Bad Identifier").unwrap(),
-    };
+    led.set_low().unwrap();
+    delay.delay_ms(250u32);
+    led.set_high().unwrap();
+    delay.delay_ms(250u32);
 
     panic!();
 }
